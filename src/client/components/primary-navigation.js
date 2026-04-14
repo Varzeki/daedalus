7eff400c0424701211ee66085aba4ba672e4d12d
varzeki <expectthespanish@gmail.com>
2026-04-14 21:26:10 +1000

    v1.8.3: Fix release pipeline, add tab memory
    - Download yt-dlp.exe in CI workflow so NSIS installer builds correctly
    - Remember last visited sub-page per tab using sessionStorage
    - Switching tabs now returns to the last sub-page instead of the default

--- src/client/components/primary-navigation.js
+++ src/client/components/primary-navigation.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const NAV_BUTTONS = [
   { name: 'Commander', abbr: 'Cmdr', path: '/cmdr' },
@@ -13,9 +14,23 @@ const NAV_BUTTONS = [
   { name: 'Controls', abbr: 'Ctrl', path: '/controls' }
 ]
 
+const TAB_MEMORY_PREFIX = 'daedalus:lastPage:'
+
 export default function PrimaryNavigation () {
   const router = useRouter()
   const currentPath = `/${router.pathname.split('/')[1].toLowerCase()}`
+
+  // Remember the current sub-page for this tab
+  useEffect(() => {
+    if (router.pathname && router.pathname !== currentPath) {
+      sessionStorage.setItem(TAB_MEMORY_PREFIX + currentPath, router.pathname)
+    }
+  }, [router.pathname])
+
+  function navigateToTab (tabPath) {
+    const lastPage = sessionStorage.getItem(TAB_MEMORY_PREFIX + tabPath)
+    router.push(lastPage || tabPath)
+  }
 
   return (
     <div id='primaryNavigation' className='button-group'>
@@ -26,7 +41,7 @@ export default function PrimaryNavigation () {
           tabIndex='1'
           disabled={button.path === currentPath}
           className={button.path === currentPath ? 'button--active' : ''}
-          onClick={() => router.push(button.path)}
+          onClick={() => navigateToTab(button.path)}
         >
           <span className='visible-small'>{button.abbr}</span>
           <span className='hidden-small'>{button.name}</span>
